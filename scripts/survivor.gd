b03e5cd85437593b05a30829e136a2c15f7e0d43
colin-every <commandnoobmc23@gmail.com>
2024-02-23 11:28:23 -0500

    some things that were not committed

--- scripts/survivor.gd
+++ scripts/survivor.gd
@@ -1,12 +1,14 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+var SPEED = 5.0
 const ACCEL = 6.0
 const DEACCEL = 8.0
 const JUMP_VELOCITY = 4.5
 
 var freeMouse = true
+@export var speedBase = 15
+@export var sprintBase = 30
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -17,13 +19,15 @@ func _enter_tree():
 	
 
 func _ready():
-	
-	
-	
 	if is_multiplayer_authority():
 		%Camera.current = true
 	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if !freeMouse else Input.MOUSE_MODE_VISIBLE;
+	
+	if name == "1":
+		speedBase = 20
+		sprintBase = 40
+		scale = Vector3(2, 2, 2)
 	
 
 func _physics_process(delta):
@@ -54,6 +58,11 @@ func move(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("sprint"):
+		SPEED = sprintBase
+	else:
+		SPEED = speedBase
 	
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")

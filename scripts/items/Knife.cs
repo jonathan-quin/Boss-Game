87a947391cffe1cda3839b65c8091e6cbb1ed232
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2024-02-26 08:32:08 -0500

    renamed something

--- scripts/items/Knife.cs
+++ scripts/items/Knife.cs
@@ -12,4 +12,10 @@ public partial class Knife : HeldItem
 	public override void _Process(double delta)
 	{
 	}
+
+	public override Use(){
+		GetNode<AnimationPlayer>("%AnimationPlayer").Play("slash");
+
+	}
+
 }

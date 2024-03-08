7bd16c33fe9ed8b6069c31576cc9e7095ee110d0
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2024-03-08 17:13:59 -0500

    Update TakeDamageInterface.cs

--- scripts/TakeDamageInterface.cs
+++ scripts/TakeDamageInterface.cs
@@ -4,6 +4,10 @@ using System;
 public partial interface TakeDamageInterface
 {
 
+	public enum TypeOfEntity {PLAYER,BOSS}
+
+	public TypeOfEntity typeOfEntity {get;set;}
+
 	public double health {get;set;}
 	public bool dead {get;set;}
 	public abstract void TakeDamage(double amount);

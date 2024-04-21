0b614be2596f65df2a0b5fe0bd35d7975c82a0a1
doegee <112405329+jonathan-quin@users.noreply.github.com>
2024-04-21 19:20:29 -0400

    removed print statements

--- scenes/UI/PlayerUI.cs
+++ scenes/UI/PlayerUI.cs
@@ -10,16 +10,16 @@ public partial class PlayerUI : Control
     [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
     public void displayMessage(string message,long id)
 	{
-		GD.Print(GetMultiplayerAuthority(), " is in the display message code. ", id);
+		//GD.Print(GetMultiplayerAuthority(), " is in the display message code. ", id);
 
 		if (id != -1)
 		{
-			GD.Print(GetMultiplayerAuthority(), " recieved a message and redirected to ", id);
+			//GD.Print(GetMultiplayerAuthority(), " recieved a message and redirected to ", id);
 			RpcId(id, "displayMessage", message,-1);
 			return;
 		}
 
-		GD.Print("running display message ", GetMultiplayerAuthority());
+		//GD.Print("running display message ", GetMultiplayerAuthority());
 
 		Tween tween = GetTree().CreateTween();
 
@@ -32,7 +32,7 @@ public partial class PlayerUI : Control
 		tween.TweenProperty(label, "visible_characters", message.Length, 0.5);
         tween.TweenProperty(label, "visible_characters", 0, 0.5).SetDelay(3.0);
 
-		GD.Print("The messaage is: ", message);
+		//GD.Print("The messaage is: ", message);
 
     }
 
